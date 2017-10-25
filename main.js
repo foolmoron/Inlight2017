0d25884dde95ebdf1e13274ff9b8c38d08739512
Momin Khan <foolmoron@gmail.com>
2017-10-25 16:53:45 -0400

    Better width picker

--- main.js
+++ main.js
@@ -176,12 +176,9 @@ window.onload = function() {
     // color controls
     var colorContainer = document.querySelector('.colors')
     var colorButtons = Array.from(document.querySelectorAll('.colors > div'))
-    var widthCircle = document.querySelector('.width-circle.main')
-    var widthCircleOutline = document.querySelector('.width-circle.outline')
-    var widthCircleOutlineInner = document.querySelector('.width-circle.outline-inner')
 
     function setColor(colorPicker) {
-        // set pickers
+        // set color pickers
         var currentIndex = colorButtons.indexOf(colorPicker)
         console.log(currentIndex)
         for (var i = 0; i < colorButtons.length; i++) {
@@ -200,8 +197,6 @@ window.onload = function() {
         var color = colorPicker.dataset.color
         canvas.freeDrawingBrush.color = color
         document.body.style.backgroundColor = color
-        widthCircle.style.backgroundColor = color
-        widthCircleOutline.style.backgroundColor = color
     }
 
     for (var i = 0; i < colorButtons.length; i++) {
@@ -212,30 +207,28 @@ window.onload = function() {
     setColor(colorButtons[0])
 
     // width controls
-    var widthInput = document.querySelector('.width-input')
+    var widthButtons = Array.from(document.querySelectorAll('.width'))
     
-    function setWidth(width, minWidth, maxWidth) {
-        canvas.freeDrawingBrush.width = width
-        var parentHeight = widthCircle.parentElement.offsetHeight
-        var percHeight = ilerp(width, minWidth, maxWidth) * 0.9 + 0.1
-        widthCircle.style.width = parentHeight*percHeight + 'px'
-        widthCircle.style.height = parentHeight*percHeight + 'px'
-        widthCircleOutline.style.width = parentHeight+2 + 'px'
-        widthCircleOutline.style.height = parentHeight+2 + 'px'
-        widthCircleOutlineInner.style.width = parentHeight + 'px'
-        widthCircleOutlineInner.style.height = parentHeight + 'px'
+    function setWidth(widthButton) {
+        // set width pickers
+        for (var i = 0; i < widthButtons.length; i++) {
+            widthButtons[i].classList.remove('selected')
+        }
+        widthButton.classList.add('selected')
+        // set width
+        canvas.freeDrawingBrush.width = parseFloat(widthButton.dataset.size)
     }
 
-    widthInput.oninput = e => setWidth(e.target.valueAsNumber, parseFloat(e.target.min), parseFloat(e.target.max))
-    setWidth(widthInput.valueAsNumber, parseFloat(widthInput.min), parseFloat(widthInput.max))
+    for (var i = 0; i < widthButtons.length; i++) {
+        widthButtons[i].onclick = e => setWidth(e.target)
+    }
+    setWidth(widthButtons[1])
 
     // resizing
-    var offsettingElements = ['.prompt', '.colors']
+    var offsettingElements = ['.prompt', '.colors', '.controls']
         .map(s => Array.from(document.querySelectorAll(s)))
         .reduce((acc, arr) => acc.concat(arr), [])
     function handleResize() {
-        // controls
-        setWidth(widthInput.valueAsNumber, parseFloat(widthInput.min), parseFloat(widthInput.max))
         // canvas
         var elementOffset = offsettingElements.reduce((acc, item) => acc + item.offsetHeight, 0)
         var extraOffset = 0
