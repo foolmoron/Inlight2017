fc14357929c21d329edecaccf0670276044b28e0
Momin Khan <foolmoron@gmail.com>
2017-10-25 01:50:23 -0400

    Read color from textures

--- Assets/Scripts/ImageReader.cs
+++ Assets/Scripts/ImageReader.cs
@@ -3,16 +3,22 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Profiling;
 using Debug = UnityEngine.Debug;
 using Random = UnityEngine.Random;
 
+[Serializable]
 public class ImageRecord {
+    public string Name;
     public string Path;
-    public Texture2D Texture = new Texture2D(2, 2);
     public Vector2 Dimensions;
     public DateTime LastUpdated;
+    public Color MainColor;
+
+    public const int MAIN_COLOR_MIP_LEVEL = 3;
+    public Texture2D Texture { get; set; }
 }
 
 public class ImageReader : Manager<ImageReader> {
@@ -48,6 +54,7 @@ public class ImageReader : Manager<ImageReader> {
     }
 
     IEnumerator MainReader() {
+        var x = new Stopwatch();
         while (true) {
             // get file index if changed
             if (File.GetLastWriteTime(indexPath) > lastIndexWrite) {
@@ -67,17 +74,25 @@ public class ImageReader : Manager<ImageReader> {
                 // create if new file in directory
                 if (record == null) {
                     record = new ImageRecord {
+                        Name = file,
                         Path = root + file + ".png",
+                        Texture = new Texture2D(2, 2),
                     };
                     Records.Add(record);
                     OnAdded(record);
                     yield return null;
                 }
                 // update file if image was changed
-                if (File.GetLastWriteTime(record.Path) > record.LastUpdated) {
-                    record.Texture.LoadImage(File.ReadAllBytes(record.Path));
-                    record.Dimensions = new Vector2(record.Texture.width / 100f, record.Texture.height / 100f);
+                if (File.Exists(record.Path) && File.GetLastWriteTime(record.Path) > record.LastUpdated) {
+                    // do all file-reading work in one atomic chunk
                     record.LastUpdated = File.GetLastWriteTime(record.Path);
+                    record.Texture.LoadImage(File.ReadAllBytes(record.Path));
+                    yield return endOfFrame;
+                    var pixels = record.Texture.GetPixels32(ImageRecord.MAIN_COLOR_MIP_LEVEL);
+                    yield return null;
+                    record.MainColor = GetMainColorFromPixels(pixels, 2); // blend top 2 colors
+                    yield return null;
+                    record.Dimensions = new Vector2(record.Texture.width / 100f, record.Texture.height / 100f);
                     OnUpdated(record);
                 }
                 yield return null;
@@ -97,6 +112,51 @@ public class ImageReader : Manager<ImageReader> {
             // wait until next frame
             yield return endOfFrame;
         }
+    }
+
+    static Color GetMainColorFromPixels(Color32[] pixels, int blendTopColors = 1) {
+        // get frequency of each solid pixel color
+        var pixelFrequencies = new Dictionary<Color32, int>();
+        foreach (var pixel in pixels) {
+            if (pixel.a > 128) {
+                var freq = pixelFrequencies.ContainsKey(pixel) ? pixelFrequencies[pixel] : 0;
+                pixelFrequencies[pixel] = freq + 1;
+            }
+        }
+        // black if nothing
+        if (pixelFrequencies.Count == 0) {
+            return Color.black;
+        }
+        // get most common colors
+        var topColors = pixelFrequencies.OrderByDescending(pair => pair.Value).Select(pair => (Color)pair.Key).ToList();
+        // blend top colors together in equal proportion
+        var finalColor = topColors[0];
+        for (int i = 1; i < topColors.Count && i < blendTopColors; i++) {
+            finalColor = Color.Lerp(finalColor, topColors[i], 1f / (i + 1));
+        }
+        // max sat/val of final color
+        finalColor = Color.HSVToRGB(HSBColor.FromColor(finalColor).h, 1, 1);
+        return finalColor;
+    }
+
+    static Color GetAverageColorFromPixels(Color[] pixels) {
+        // blank pixels to start
+        var r = 0f;
+        var g = 0f;
+        var b = 0f;
+        // each pixel adds to rgb based on the ratio of pixel alpha to total alpha of all pixels
+        var totalAlpha = pixels.Sum(c => c.a);
+        foreach (var pixel in pixels) {
+            var weight = pixel.a / totalAlpha;
+            r += pixel.r * weight;
+            g += pixel.g * weight;
+            b += pixel.b * weight;
+        }
+        // max sat/bright of color
+        var hsbColor = HSBColor.FromColor(new Color(r, g, b));
+        hsbColor.s = 1;
+        hsbColor.b = 1;
+        return hsbColor.ToColor();
     }
 
     public ImageRecord GetWeightedRandomRecord() {
