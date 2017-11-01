9b46d8e324352b5ba322536b0a604f3c9f0c3ede
Momin Khan <foolmoron@gmail.com>
2017-11-01 00:27:00 -0400

    Kill floor

---
+++ Assets/Scripts/DieBelowY.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DieBelowY : MonoBehaviour {
+
+    public float Y;
+    public bool Release;
+    
+    void Update() {
+        if (transform.position.y < 0) {
+            if (Release) {
+                gameObject.Release();
+            } else {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
