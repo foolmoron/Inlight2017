19d608cc1c4f25c6f277a6962ffe562d22d5f5dd
Momin Khan <foolmoron@gmail.com>
2017-11-02 01:12:20 -0400

    video overlay

---
+++ Assets/Scripts/VideoToImage.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class VideoToImage : MonoBehaviour {
+
+    RawImage image;
+    VideoPlayer video;
+    
+    void Start() {
+        image = GetComponent<RawImage>();
+        video = GetComponent<VideoPlayer>();
+
+        var tex = new RenderTexture((int)video.clip.width, (int)video.clip.height, 0);
+        video.targetTexture = tex;
+        image.texture = tex;
+    }
+}
