3afb6123abd3706aa20b50fdb25819e52e907983
Momin Khan <foolmoron@gmail.com>
2018-03-25 23:34:20 -0400

    Kill animals after 1 min + material changes

--- Assets/Scripts/SpawnedObject.cs
+++ Assets/Scripts/SpawnedObject.cs
@@ -5,6 +5,7 @@ public class SpawnedObject : MonoBehaviour {
 
     public ImageRecord Record;
     public bool ZAligned;
+    Texture2D prevTex;
 
     [Range(0, 10)]
     public float ScaleFactor = 1;
@@ -14,6 +15,10 @@ public class SpawnedObject : MonoBehaviour {
     public float ScaleSpeed = 0.9f;
     public bool UseScaleSpeed = true;
     public Transform ScaleTarget;
+    
+    public AnimationCurve DieCurve;
+    public float LifeTime;
+    float dieScaleFactor;
 
     Vector2 originalScale;
     new Renderer renderer;
@@ -28,27 +33,37 @@ public class SpawnedObject : MonoBehaviour {
         renderer.material.EnableKeyword("_EMISSION");
     }
 
+    void OnObtain() {
+        LifeTime = 0;
+    }
+
     void FixedUpdate() {
         if (UseScaleSpeed) {
             ScaleFactor = Mathf.Lerp(ScaleFactor, TargetScale, ScaleSpeed);
+        }
+        LifeTime += Time.deltaTime;
+        dieScaleFactor = DieCurve.Evaluate(LifeTime);
+        if (dieScaleFactor <= 0) {
+            gameObject.Release();
         }
     }
 
     void Update() {
         if (Record != null) {
             if (Record.Dimensions != Vector2.zero && !ZAligned)
-                ScaleTarget.localScale = new Vector3(originalScale.x * Record.Dimensions.aspect(), originalScale.y, 1) * ScaleFactor;
+                ScaleTarget.localScale = new Vector3(originalScale.x * Record.Dimensions.aspect(), originalScale.y, 1) * ScaleFactor * dieScaleFactor;
             if (Record.Dimensions != Vector2.zero && ZAligned)
-                ScaleTarget.localScale = new Vector3(1, originalScale.y, originalScale.x * Record.Dimensions.aspect()) * ScaleFactor;
-            if (Record.Texture) {
+                ScaleTarget.localScale = new Vector3(1, originalScale.y, originalScale.x * Record.Dimensions.aspect()) * ScaleFactor * dieScaleFactor;
+            if (Record.Texture && Record.Texture != prevTex) {
                 renderer.material.mainTexture = Record.Texture;
                 renderer.material.mainTextureScale = Record.Facing == ImageFacing.Left ? Vector2.one : new Vector2(-1, 1);
                 renderer.material.SetTexture("_EmissionMap", Record.Texture);
                 renderer.material.SetColor("_EmissionColor", Color.white);
             }
+            prevTex = Record.Texture;
         }
         if (Record == null || Record.Dimensions == Vector2.zero) {
-            ScaleTarget.localScale = Vector3.one * ScaleFactor;
+            ScaleTarget.localScale = Vector3.one * ScaleFactor * dieScaleFactor;
         }
     }
 }
