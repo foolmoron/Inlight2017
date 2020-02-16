e8d8f5a3f895a13f7594b661f4b5d3beabe11eb4
Momin Khan <foolmoron@gmail.com>
2020-02-16 18:18:33 -0500

    Update command reader to new Unity web API

--- Assets/Scripts/CommandReader.cs
+++ Assets/Scripts/CommandReader.cs
@@ -2,8 +2,7 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.UI;
-using UnityEngine.Video;
+using UnityEngine.Networking;
 
 [Serializable]
 public class CommandMeta {
@@ -25,7 +24,7 @@ public class CommandDataContainer {
 
 public class CommandReader : Manager<CommandReader> {
 
-    public string BaseUrl = @"https://inlight.fool.games/";
+    public string BaseUrl = @"https://lightfield.fool.games/";
 
     [Range(0, 3)]
     public float PollInterval = 1f;
@@ -33,7 +32,7 @@ public class CommandReader : Manager<CommandReader> {
 
 	[Range(0, 20)]
 	public float WiggleTime = 5f;
-	ListDict<ImageRecord, float> wiggleTimes = new ListDict<ImageRecord, float>();
+	readonly ListDict<ImageRecord, float> wiggleTimes = new ListDict<ImageRecord, float>();
 
     void Start() {
         StartCoroutine(Poll());
@@ -41,40 +40,38 @@ public class CommandReader : Manager<CommandReader> {
 
     IEnumerator Poll() {
         while (true) {
-            var req = new WWW(string.Format("{0}command/{1}", BaseUrl, latestPollTime));
-            yield return req;
-			var clearReqs = new List<WWW>(10);
+			var req = UnityWebRequest.Get(string.Format("{0}command/{1}", BaseUrl, latestPollTime));
+            yield return req.SendWebRequest();
+			var clearReqs = new List<UnityWebRequest>(10);
 			if (req.error == null) {
 				var firstLoad = latestPollTime == 0;
-				var data = JsonUtility.FromJson<CommandDataContainer>(req.text);
+				var data = JsonUtility.FromJson<CommandDataContainer>(req.downloadHandler.text);
 				latestPollTime = data.time;
 				if (!firstLoad) { // wait until 2nd poll so we know we are pulling using the correct time
 					// process commands
 					foreach (var command in data.commands) {
-						if (command.num > 0) {
-							// do command
-							switch (command.type) {
-								case CommandType.WIGGLE:
-									Debug.LogError("WIGGLING " + command.uuid);
-									var record = ImageReader.Inst.Records.Find(command.uuid, (r, uuid) => r.Name == uuid);
-									if (record != null) {
-										foreach (var obj in SpawnedObject.AllInCurrentScene) {
-											if (obj.Record.Record == record) {
-												obj.IsWiggling = true;
-												obj.Update();
-											}
+						// do command
+						switch (command.type) {
+							case CommandType.WIGGLE:
+								Debug.LogError("WIGGLING " + command.uuid);
+								var record = ImageReader.Inst.Records.Find(command.uuid, (r, uuid) => r.Name == uuid);
+								if (record != null) {
+									foreach (var obj in SpawnedObject.AllInCurrentScene) {
+										if (obj.Record.Record == record) {
+											obj.IsWiggling = true;
+											obj.Update();
 										}
-										wiggleTimes[record] = WiggleTime;
 									}
-									break;
-							}
-							// clear command
-							clearReqs.Add(new WWW(string.Format("{0}command/{1}/{2}/clear", BaseUrl, command.type, command.uuid)));
+									wiggleTimes[record] = WiggleTime;
+								}
+								break;
 						}
+						// clear command
+						clearReqs.Add(UnityWebRequest.Get(string.Format("{0}command/{1}/{2}/clear", BaseUrl, command.type, command.uuid)));
 					}
 					// wait for clears
 					foreach (var clearReq in clearReqs) {
-						yield return clearReq;
+						yield return clearReq.SendWebRequest();
 					}
 					clearReqs.Clear();
 				}
