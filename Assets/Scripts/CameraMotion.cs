efd6bf6c90d9d28d44c9b44acf60e1bd6d4da3d9
Momin Khan <foolmoron@gmail.com>
2020-02-28 07:48:16 -0500

    Shaky cam + tweaked spawn rates/sizes

--- Assets/Scripts/CameraMotion.cs
+++ Assets/Scripts/CameraMotion.cs
@@ -13,6 +13,13 @@ public class CameraMotion : MonoBehaviour {
     public float RotationIntervalVariance = 0.5f;
     float timeToInterval;
 
+    public Vector3 AngleRange2 = new Vector3(1, 1, 0);
+    [Range(0, 1)]
+    public float RotationInterval2 = 0.05f;
+    [Range(0, 0.1f)]
+    public float RotationIntervalVariance2 = 0.5f;
+    float timeToInterval2;
+
     public bool Zoomed;
     public Vector2 ZoomAmounts = new Vector2(13.1f, 34.3f);
     public Vector2 ZoomChances = new Vector2(0.1f, 0.5f);
@@ -36,6 +43,11 @@ public class CameraMotion : MonoBehaviour {
         if (timeToInterval <= 0) {
             timeToInterval = RotationInterval * (1 + (Random.value - 0.5f) * RotationIntervalVariance);
             targetRotation = originalRotation * Quaternion.Euler(new Vector3((Random.value - 0.5f) * AngleRange.x, (Random.value - 0.5f) * AngleRange.y, (Random.value - 0.5f) * AngleRange.z));
+        }
+        timeToInterval2 -= Time.deltaTime;
+        if (timeToInterval2 <= 0) {
+            timeToInterval2 = RotationInterval2 * (1 + (Random.value - 0.5f) * RotationIntervalVariance2);
+            targetRotation = targetRotation * Quaternion.Euler(new Vector3((Random.value - 0.5f) * AngleRange2.x, (Random.value - 0.5f) * AngleRange2.y, (Random.value - 0.5f) * AngleRange2.z));
         }
         transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, RotationSpeed);
 
