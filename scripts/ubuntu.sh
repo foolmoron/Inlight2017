fc7e63ab48b50a696903bc753f3803d088c665e3
Momin Khan <foolmoron@gmail.com>
2020-09-05 18:08:36 +0000

    new certbot script

--- scripts/ubuntu.sh
+++ scripts/ubuntu.sh
@@ -14,7 +14,7 @@ git clone -b server https://github.com/foolmoron/Inlight2017.git
 cd Inlight2017/
 npm install
 mkdir certs
-sudo certbot certonly --standalone -w certs -d lightfield.fool.games
+sudo certbot certonly --manual --preferred-challenges dns -w certs -d lightfield.fool.games
 
 echo "1. Change ports to 80/443"
 echo "2. Change config.js secret/pass/host"
